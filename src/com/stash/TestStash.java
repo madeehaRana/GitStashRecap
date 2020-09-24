a1ffbb5c8a7f133273ab1b65dbc0628cfd05743d
madeehaRana <madeeharizwan786@gmail.com>
2020-09-24 19:45:32 -0400

    first commit

---
+++ src/com/stash/TestStash.java
@@ -0,0 +1,9 @@
+package com.stash;
+
+public class TestStash {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/stash/class1/TestStash.java
@@ -0,0 +1,10 @@
+package com.stash.class1;
+
+public class TestStash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
