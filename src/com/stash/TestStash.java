60a9f2fa2f7cd82ce8ecb04c770041cd135adc6b
madeehaRana <madeeharizwan786@gmail.com>
2020-09-24 19:57:27 -0400

    TestStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -3,7 +3,8 @@ package com.stash;
 public class TestStash {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Modification of our code");
+		System.out.println("Hello World");
 	}
 
 }
