bdfeabf5e685f22f9d870746ba898d1677d721c6
madeehaRana <madeeharizwan786@gmail.com>
2020-09-24 21:29:11 -0400

    FeatureB completed

---
+++ src/com/stash/featureB.java
@@ -0,0 +1,11 @@
+package com.stash;
+
+public class featureB {
+
+	public static void main(String[] args) {
+		System.out.println("FeatureB");
+		System.out.println("Feature B completed");
+
+	}
+
+}
